e9bb6bdb163c35ae7c7532ae012d590d99cc4c37
Nils <github@d4d1.de>
2021-01-27 21:38:25 +0100

    simplifying the test methods using the __eq__() method

--- test_paramodulation.py
+++ test_paramodulation.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 import clauses
+import literals
 import paracontrol
 import position
 import rewriterule
@@ -43,39 +44,42 @@ cnf(prove_equation,negated_conjecture,
     def test_find_should_succeed_term_is_found(self):
         pos = self.c1.find(['add', 'A', 'B'])
         pos2 = self.c2.find(['add', 'A', 'B'])
+        subst1 = substitutions.Substitution()
+        subst2 = substitutions.Substitution([('B', 'A')])
+        subst3 = substitutions.Substitution([('B', 'A'), ('C', 'A')])
+        expectedPos1 = position.Position(subst1, [0, 1])
+        expectedPos2 = position.Position(subst2, [0, 2])
+        expectedPos3 = position.Position(subst3, [0, 1, 2])
+        expectedPos4 = position.Position(subst1, [0, 2, 1])
 
         self.assertEqual(2, len(pos))
-        self.assertEqual([0, 1], pos[0].l)
-        self.assertEqual({}, pos[0].get_unifier().subst)
-        self.assertEqual([0, 2], pos[1].l)
         self.assertEqual({'B': 'A'}, pos[1].get_unifier().subst)
+        self.assertEqual(expectedPos1, pos[0])
+        self.assertEqual(expectedPos2, pos[1])
 
         self.assertEqual(2, len(pos2))
-        self.assertEqual([0, 1, 2], pos2[0].l)
-        self.assertEqual({'B': 'A', 'C': 'A'}, pos2[0].get_unifier().subst)
-        self.assertEqual([0, 2, 1], pos2[1].l)
-        self.assertEqual({}, pos2[1].get_unifier().subst)
+        self.assertEqual(expectedPos3, pos2[0])
+        self.assertEqual(expectedPos4, pos2[1])
 
     def test_createRewriteRule_should_succeed_if_rule_is_created(self):
         rewrite_rules1 = paracontrol.createRewriteRule(self.c1)
         rewrite_rules2 = paracontrol.createRewriteRule(self.c4)
+
+        expectedLiteral1 = literals.Literal(['=', 'A', 'B'], True)
+        expectedLiteral2 = literals.Literal(['=', 'C', ['add', 'A', 'D']], True)
+        expectedLiterals = [expectedLiteral1, expectedLiteral2]
+        expectedRewriteRule1 = rewriterule.rewriteRule(['add', 'A', 'B'], ['add', 'B', 'A'], [], self.c1)
+        expectedRewriteRule2 = rewriterule.rewriteRule(['add', 'B', 'A'], ['add', 'A', 'B'], [], self.c1)
+        expectedRewriteRule3 = rewriterule.rewriteRule('C', ['add', 'B', 'D'], expectedLiterals, self.c4)
+        expectedRewriteRule4 = rewriterule.rewriteRule(['add', 'B', 'D'], 'C', expectedLiterals, self.c4)
+
         self.assertEqual(2, len(rewrite_rules1))
-        self.assertEqual(['add', 'A', 'B'], rewrite_rules1[0].get_from())
-        self.assertEqual(['add', 'B', 'A'], rewrite_rules1[0].get_to())
-        self.assertEqual(['add', 'B', 'A'], rewrite_rules1[1].get_from())
-        self.assertEqual(['add', 'A', 'B'], rewrite_rules1[1].get_to())
-        self.assertEqual([], rewrite_rules1[0].get_literals())
-        self.assertEqual(self.c1, rewrite_rules1[0].clause)
+        self.assertEqual(expectedRewriteRule1, rewrite_rules1[0])
+        self.assertEqual(expectedRewriteRule2, rewrite_rules1[1])
 
         self.assertEqual(2, len(rewrite_rules2))
-        self.assertEqual('C', rewrite_rules2[0].get_from())
-        self.assertEqual(['add', 'B', 'D'], rewrite_rules2[0].get_to())
-        self.assertEqual(['add', 'B', 'D'], rewrite_rules2[1].get_from())
-        self.assertEqual('C', rewrite_rules2[1].get_to())
-        self.assertEqual(2, len(rewrite_rules2[0].get_literals()))
-        self.assertEqual(['=', 'A', 'B'], rewrite_rules2[0].get_literals()[0].atom)
-        self.assertEqual(['=', 'C', ['add', 'A', 'D']], rewrite_rules2[0].get_literals()[1].atom)
-        self.assertEqual(self.c4, rewrite_rules2[0].clause)
+        self.assertEqual(expectedRewriteRule3, rewrite_rules2[0])
+        self.assertEqual(expectedRewriteRule4, rewrite_rules2[1])
 
     def test_createRewriteRule_should_succeed_if_no_rule_created(self):
         rewrite_rules = paracontrol.createRewriteRule(self.c5)
@@ -85,13 +89,15 @@ cnf(prove_equation,negated_conjecture,
         subst = substitutions.Substitution()
         pos = position.Position(subst, [0, 1, 1, ])
         clause = self.c3.replaceSubstitute(pos, ['add', 'B', 'A'])
-        self.assertEqual(['=', ['subtract', ['add', 'B', 'A'], 'C'], ['add', ['subtract', 'A', 'C'], 'B']],
-                         clause.literals[0].atom)
+        expectedLiteral = literals.Literal(['=', ['subtract', ['add', 'B', 'A'], 'C'], ['add', ['subtract', 'A', 'C'], 'B']])
+        expectedClause = clauses.Clause([expectedLiteral])
+        self.assertEqual(expectedClause, clause)
 
     def test_apply_should_succeed_if_new_clauses_are_correct(self):
         to = ['add', 'B', 'A']
         frm = ['add', 'A', 'B']
+        expectedLiteral = literals.Literal(['=', ['subtract', ['add', 'B', 'A'], 'C'], ['add', ['subtract', 'A', 'C'], 'B']])
+        expectedClause = clauses.Clause([expectedLiteral])
         rewrite_rule = rewriterule.rewriteRule(frm, to, [], self.c1)
         res = rewrite_rule.apply(self.c3)
-        self.assertEqual(['=', ['subtract', ['add', 'B', 'A'], 'C'], ['add', ['subtract', 'A', 'C'], 'B']],
-                         res[0].getLiteral(0).atom)
+        self.assertEqual(expectedClause, res[0])
